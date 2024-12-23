26715f5adea4dec99549ebfa6d69ece840a24c95
Tạ Hải Lượng <10147245-luongth@users.noreply.gitlab.com>
2024-12-23 08:21:36 +0700

    23/12/20224

---
+++ src/main/java/com/building_mannager_system/entity/property_manager/DeviceType.java
@@ -0,0 +1,25 @@
+package com.building_mannager_system.entity.property_manager;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "device_type")
+public class DeviveType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "device_type_id", nullable = false)
+    private Long id;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "type_name", nullable = false)
+    private String typeName;
+
+}
